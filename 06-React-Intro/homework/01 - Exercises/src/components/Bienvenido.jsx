b5558b1340cbdfb6031a62978b0ba06f39c29113
FacuTombesi <facundomtombesi@gmail.com>
2022-11-28 15:18:30 -0300

    06 homework

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from "react"
 
-const studentName = "";
+const studentName = "Facundo Tombesi";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Homework 06 Intro React</h1>
+      <h3>{studentName}</h3>
+      <ul>{techSkills.map()}</ul>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
