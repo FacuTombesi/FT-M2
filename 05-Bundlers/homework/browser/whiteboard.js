d78f89986bb4c7a5678cc800eedd856a12278a33
FacuTombesi <facundomtombesi@gmail.com>
2022-11-28 09:39:08 -0300

    05 homework

--- 05-Bundlers/homework/browser/whiteboard.js
+++ 05-Bundlers/homework/browser/whiteboard.js
@@ -1,6 +1,8 @@
-(function () {
+// window.whiteboard = new window.EventEmitter();
 
-  window.whiteboard = new window.EventEmitter();
+const EventEmitter = require("./event-emitter.js");
+
+const whiteboard = new EventEmitter;
 
   // Ultimately, the color of our stroke;
   var color;
@@ -113,4 +115,4 @@
 
   };
 
-})();
+module.exports = whiteboard;
