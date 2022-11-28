4b4fa75ec0bc53e43e1648c82158f8dd0a61b2b1
FacuTombesi <facundomtombesi@gmail.com>
2022-11-28 16:46:11 -0300

    06 homework

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,11 +1,11 @@
 export default function Card(props) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img  src={} alt="" /> */}
+         {/* <button onClick={}>X</button> */}
+         <h2>{Rick.name}</h2>
+         <h2>{Rick.species}</h2>
+         <h2>{Rick.gender}</h2>
+         <img  src={Rick.image} alt="Card" />
       </div>
    );
 }
